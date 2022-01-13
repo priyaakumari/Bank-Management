22ca97325682af4faafbf2150eaded6a9b116b19
Priya Kumari <priyaprasad.407gmail.com>
2022-01-14 00:14:57 +0530

    changes

--- New Pro/src/Main.java
+++ New Pro/src/Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=9;
+		int b=19;
+		int c=a+b;
+		System.out.println(c);
 	}
 
 }
---
+++ New Pro/src/priya.java
@@ -0,0 +1,9 @@
+
+public class priya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
